0b450f39710a6db19b80c590214f697b9137255e
SuttonB-000 <suttonB-000@protonmail.com>
2026-04-22 18:50:51 -0400

    repairs  to enable github pages use

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/sdev-255-react-group-proj',
+  assetPrefix: '/sdev-255-react-group-proj/',
+};
+
+module.exports = nextConfig;
