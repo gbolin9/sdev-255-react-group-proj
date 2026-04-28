548135b5cd21ac8fdbfd96479af2b602fdf82283
gary bolin <gbolin@ivytech.edu>
2026-04-28 08:17:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
  // output: 'export',
   basePath: '/sdev-255-react-group-proj',
   assetPrefix: '/sdev-255-react-group-proj/',
-  images: {
-    unoptimized: true,
   },
   // reactCompiler: true, // Optional: Enable if using the new React Compiler
 };
