1ca6f58ecc4d0814b68670353c7ac41b68723696
gary bolin <gbolin@ivytech.edu>
2026-04-28 07:27:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   basePath: '/sdev-255-react-group-proj',
   assetPrefix: '/sdev-255-react-group-proj/',
 };
